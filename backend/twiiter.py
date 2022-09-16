9297719ef16768c9939f50c88271d2b161fcc2c5
gamingflexer <osurve20ece@student.mes.ac.in>
2022-09-17 01:40:26 +0530

    translation added

--- backend/twiiter.py
+++ backend/twiiter.py
@@ -3,8 +3,9 @@ import os
 import json
 import uuid
 from config import tweetpath
+from deep_translator import GoogleTranslator
 import pandas as pd
-
+import time
 
 def top_tweets(username):
     for user in username:
@@ -31,5 +32,17 @@ def top_tweets(username):
     data_sorted = pd.read_json(data)
     data_sorted = data_sorted.drop(['mentions','created_at','date', 'user_rt_id', 'user_rt', 'retweet_id', 'reply_to', 'retweet_date', 'trans_src',
                      'trans_dest', 'cashtags', 'retweet', 'quote_url', 'place', 'conversation_id', 'id'], axis=1)
+    data_sorted['translated'] = data_sorted.apply(lambda x: g_translation_function_en(data_sorted['tweet']))
     print('\nTwitter data scrapped !\n')
     return data_sorted.to_json(orient='records')
+
+def g_translation_function_en(inText):
+  try:
+    if len(inText)<=4999:
+      outText = GoogleTranslator(source='auto', target='en').translate(inText)
+      return outText
+    else:
+      return inText
+  except Exception as e:
+    print(e)
+    return inText
