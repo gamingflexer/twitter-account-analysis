346555dc8e8ca763f8a1a2a9b61b47241e511ea4
gamingflexer <osurve20ece@student.mes.ac.in>
2022-09-17 01:32:35 +0530

    minor fixes | twiiter data api

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,28 @@
-from flask import Flask,redirect,url_for,render_template
+from flask import Flask,render_template
 from twiiter import top_tweets
 import json
 
-app =Flask(__name__,static_folder='static') 
+app = Flask(__name__, static_folder='static')
+
 
 @app.route('/')
-def home(): 
+def home():
     return render_template('home_page.html')
+
 
 @app.route('/secondpage')
 def secondpage():
-    return render_template('2ndpage.html') 
+    return render_template('2ndpage.html')
 
-@app.route('/twitter')
-def twitter():
-    data = top_tweets(["EvilonMusk"])
-    return  json.dumps(data)
- 
+
+@app.route('/twitter=<username>')
+def twitter(username):
+    try:
+        data = json.loads(top_tweets([username]))
+    except FileNotFoundError:
+        data = json.loads(top_tweets([username]))
+    return json.dumps(data[:2])
+
+
 if __name__ == '__main__':
     app.run(debug=True)
